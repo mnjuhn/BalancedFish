be3ce87784f23e67db529e10ec398f3c827a8c88
Ben Juhn <bjuhn@apple.com>
2018-02-10 16:46:17 -0500

    more updates

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,48 @@
+var ingredientNutrition = { 
+    Overall: {
+        columns: [
+            ['Protein', 30],
+            ['NFE (Carbs)', 120],
+            ['Fat', 120],
+            ['Ash', 120],
+            ['Other', 120]
+        ]
+    },
+    Insects: {
+        columns: [
+            ['Protein', 11],
+            ['NFE (Carbs)', 141],
+            ['Fat', 120],
+            ['Ash', 22],
+            ['Other', 33]
+        ]
+    },
+    'Fish Meal': {
+        columns: [
+            ['Protein', 12],
+            ['NFE (Carbs)', 67],
+            ['Fat', 77],
+            ['Ash', 25],
+            ['Other', 10]
+        ]
+    }
+}
+
+
 var chart1 = c3.generate({
     bindto: '#chart1',   
     data: {
-        columns: [
-            ['Soy Meal X', 30],
-            ['Fish Meal Y', 120],
-            ['Amino Acid Mix A', 120]
-        ],
+        columns: ingredientNutrition.Overall.columns,
         type : 'donut',
-        onclick: function (d, i) { console.log("onclick", d, i); },
+        onclick: function (d, i) { 
+            console.log("onclick", d, i); 
+            chart2.load(d);
+        },
         onmouseover: function (d, i) { console.log("onmouseover", d, i); },
         onmouseout: function (d, i) { console.log("onmouseout", d, i); }
     },
     donut: {
-        title: "Iris Petal Width"
+        title: ""
     }
 });
 
@@ -20,17 +50,24 @@ var chart2 = c3.generate({
     bindto: '#chart2',   
     data: {
         columns: [
-            ['Soy Meal X', 30],
-            ['Fish Meal Y', 120],
-            ['Amino Acid Mix A', 120]
+            ['Insects', 30],
+            ['Fish Meal', 120],
+            ['Soy Meal', 120],
+            ['Cyclops', 120],
+            ['Daphnia', 120],
+            ['Salmon Pellets', 120],
+            ['Carp Pellets', 120]
         ],
         type : 'donut',
-        onclick: function (d, i) { console.log("onclick", d, i); },
+        onclick: function (d, i) { 
+            console.log("onclick", d, i); 
+            chart1.load(ingredientNutrition[d.id]);
+        },
         onmouseover: function (d, i) { console.log("onmouseover", d, i); },
         onmouseout: function (d, i) { console.log("onmouseout", d, i); }
     },
     donut: {
-        title: "Iris Petal Width"
+        title: ""
     }
 });
 
